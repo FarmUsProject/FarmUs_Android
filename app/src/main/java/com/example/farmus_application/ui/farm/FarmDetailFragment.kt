0d6c349bd02a221cd8e998a1237886b1db8163db
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-08 23:42:27 +0900

    Fix: Farm Detail UI를 Fragment로 변경

---
+++ app/src/main/java/com/example/farmus_application/ui/farm/FarmDetailFragment.kt
@@ -0,0 +1,27 @@
+package com.example.farmus_application.ui.farm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.farmus_application.databinding.FragmentFarmDetailBinding
+
+class FarmDetailFragment : Fragment() {
+
+    private lateinit var farmDetailBinding: FragmentFarmDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
